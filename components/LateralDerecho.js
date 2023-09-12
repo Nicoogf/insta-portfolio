404e0da8bafd66e0840f030beaee991c2fa4d319
Nicoogf <nicolasgfalabella@gmail.com>
2023-09-12 15:22:32 -0300

    Avance sobre menu derecho

--- components/LateralDerecho.js
+++ components/LateralDerecho.js
@@ -4,24 +4,24 @@ import Link from "next/link";
 
 const LateralDerecho = () => {
   return (
-    <div className="border-4 border-black hidden xl:flex xl:w-3/12 flex-col justify-start">
+    <div className="hidden  bg-black xl:flex xl:w-3/12 flex-col justify-start">
 
       {/* Perfil */}
 
-      <div className="w-[100%] flex flex-row mt-10 items-center justify-center gap-x-2 mx-0 mb-6">
+      <div className="w-[100%] flex flex-row items-center justify-start gap-x-2 mt-14 mx-0 mb-6">
 
         <div>
         <Image src={"/profile.png"} width={50} height={50} alt="50" className="rounded-full mr-1"></Image>
         </div>
 
         <div>
-         <h6 className="-p-1 -my-2 text-sm font-semibold"> Desarrollador </h6> 
-         <h6 className="-p-1 -my-2 text-sm"> Front-end</h6>
+         <h6 className="-p-1 -my-2 text-sm font-semibold text-white"> Desarrollador </h6> 
+         <h6 className="-p-1 -my-2 text-sm text-gray-400"> Front-end</h6>
         </div>
 
         <div>
           <Link  href={"https://www.instagram.com/javascript.js/?hl=es"}    
-                 className='ml-8 text-sm'>
+                 className='ml-8 text-sm font-semibold text-verificado'>
 
                   Seguir
                    
@@ -34,25 +34,24 @@ const LateralDerecho = () => {
 
       <div className="w-[100%] flex flex-col mt-10 items-start ml-2">
 
-        <h5 className="font-semibold text-gray-500 text-md mb-2"> Manejo de </h5>
+        <h5 className="font-semibold text-gray-400 text-md mb-6"> Manejo de </h5>
 
         <div className="flex flex-row w-[85%] items-center justify-between gap-x-4 mb-3">
 
           <div className="flex flex-row items-center gap-x-4">
-            <Image src={"/html.jpg"} height={50} width={50} alt='js' className="rounded-full" />
+            <Image src={"/stack.jpg"} height={50} width={50} alt='js' className="rounded-full" />
             
             <div>
-            <h5 className="text-md -p-1 -my-1">HTML - CSS </h5>
-            <h6 className="text-xs -p-1 -my-1">Stack Inicial</h6>
+            <h5 className="text-md -p-1 -my-1 text-white">HTML - CSS </h5>
+            <h6 className="text-xs -p-1 -my-1 text-gray-400">Stack Inicial</h6>
             </div>
             
           </div>
 
           
-
           <div>
           <Link  href={"https://www.instagram.com/javascript.js/?hl=es"}    
-                 className={'text-sm ml-4'}>
+                 className={'text-sm ml-4 text-verificado'}>
 
                   Seguir
                    
@@ -61,20 +60,22 @@ const LateralDerecho = () => {
 
         </div>
 
-        <div className="flex flex-row w-[100%] items-center justify-start gap-x-4 mb-3">
+        <div className="flex flex-row w-[85%] items-center justify-between gap-x-4 mb-3">
 
-          <div>
-            <Image src={"/javascript.png"} height={50} width={50} alt='js' className="rounded-full" />
+          <div className="flex flex-row items-center gap-x-4">
+            <Image src={"/js.jpg"} height={50} width={50} alt='js' className="rounded-full" />
+            
+            <div>
+            <h5 className="text-md -p-1 -my-1 text-white">Javascript</h5>
+            <h6 className="text-xs -p-1 -my-1 text-gray-400">Dialecto estándar - ECMAScript</h6>
+            </div>
+            
           </div>
 
-          <div>
-            <h5 className="text-md -p-1 -my-1">Tailwind</h5>
-            <h6 className="text-xs -p-1 -my-1">Framework de CSS</h6>
-          </div>
-
+          
           <div>
           <Link  href={"https://www.instagram.com/javascript.js/?hl=es"}    
-                 className={'text-sm ml-4'}>
+                 className={'text-sm ml-4 text-verificado'}>
 
                   Seguir
                    
@@ -83,20 +84,22 @@ const LateralDerecho = () => {
 
         </div>
 
-        <div className="flex flex-row w-[100%] items-center justify-start gap-x-4 mb-3">
+        <div className="flex flex-row w-[85%] items-center justify-between gap-x-4 mb-3">
 
-          <div>
-            <Image src={"/javascript.png"} height={50} width={50} alt='js' className="rounded-full" />
+          <div className="flex flex-row items-center gap-x-4">
+            <Image src={"/react.jpg"} height={50} width={50} alt='js' className="rounded-full" />
+            
+            <div>
+            <h5 className="text-md -p-1 -my-1 text-white">React Js</h5>
+            <h6 className="text-xs -p-1 -my-1 text-gray-400">Creacion de interfaces de usuario</h6>
+            </div>
+            
           </div>
 
-          <div>
-            <h5 className="text-md -p-1 -my-1">ReactJS</h5>
-            <h6 className="text-xs -p-1 -my-1">Crearcion de interfaces</h6>
-          </div>
-
+          
           <div>
           <Link  href={"https://www.instagram.com/javascript.js/?hl=es"}    
-                 className={'text-sm ml-4'}>
+                 className={'text-sm ml-4 text-verificado'}>
 
                   Seguir
                    
@@ -105,20 +108,22 @@ const LateralDerecho = () => {
 
         </div>
 
-        <div className="flex flex-row w-[100%] items-center justify-start gap-x-4 mb-3">
+        <div className="flex flex-row w-[85%] items-center justify-between gap-x-4 mb-3">
 
-          <div>
-            <Image src={"/javascript.png"} height={50} width={50} alt='js' className="rounded-full" />
-          </div>
+        <div className="flex flex-row items-center gap-x-4">
+            <Image src={"/node.jpg"} height={50} width={50} alt='js' className="rounded-full" />
+            
+            <div>
+            <h5 className="text-md -p-1 -my-1 text-white">NodeJs</h5>
+            <h6 className="text-xs -p-1 -my-1 text-gray-400">Entorno de ejecución multiplataforma</h6>
+            </div>
+            
+         </div>
 
-          <div>
-            <h5 className="text-md -p-1 -my-1">NodeJS</h5>
-            <h6 className="text-xs -p-1 -my-1">Entorno de ejecución </h6>
-          </div>
-
+          
           <div>
           <Link  href={"https://www.instagram.com/javascript.js/?hl=es"}    
-                 className={'text-sm ml-4'}>
+                 className={'text-sm ml-4 text-verificado'}>
 
                   Seguir
                    
@@ -127,28 +132,58 @@ const LateralDerecho = () => {
 
         </div>
 
-        <div className="flex flex-row w-[100%] items-center justify-start gap-x-4 mb-3">
+        <div className="flex flex-row w-[85%] items-center justify-between gap-x-4 mb-3">
 
-          <div>
-            <Image src={"/javascript.png"} height={50} width={50} alt='js' className="rounded-full" />
+          <div className="flex flex-row items-center gap-x-4">
+              <Image src={"/tailwind.jpg"} height={50} width={50} alt='js' className="rounded-full" />
+              
+              <div>
+              <h5 className="text-md -p-1 -my-1 text-white">Tailwind CSS</h5>
+              <h6 className="text-xs -p-1 -my-1 text-gray-400">Framework de CSS</h6>
+              </div>
+              
           </div>
 
-          <div>
-            <h5 className="text-md -p-1 -my-1">NextJs</h5>
-            <h6 className="text-xs -p-1 -my-1">Marco web de desarrollo</h6>
-          </div>
+            
+            <div>
+            <Link  href={"https://www.instagram.com/javascript.js/?hl=es"}    
+                  className={'text-sm ml-4 text-verificado'}>
 
+                    Seguir
+                    
+            </Link>
+            </div>
+
+        </div>
+
+        <div className="flex flex-row w-[85%] items-center justify-between gap-x-4 mb-3">
+
+        <div className="flex flex-row items-center gap-x-4">
+            <Image src={"/next.jpg"} height={50} width={50} alt='js' className="rounded-full" />
+            
+            <div>
+            <h5 className="text-md -p-1 -my-1 text-white">Next.js</h5>
+            <h6 className="text-xs -p-1 -my-1 text-gray-400">Marco web de desarrollo front-end</h6>
+            </div>
+            
+        </div>
+
+          
           <div>
           <Link  href={"https://www.instagram.com/javascript.js/?hl=es"}    
-                 className={'text-sm ml-4'}>
+                className={'text-sm ml-4 text-verificado'}>
 
                   Seguir
-                   
+                  
           </Link>
           </div>
 
         </div>
 
+
+        
+
+    
       </div>
 
 
